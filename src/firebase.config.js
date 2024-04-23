eba7f1d8b7d022829210ac475581eb9fdfd1647a
Aashutosh <ashutoshg1101@gmail.com>
2024-04-23 11:08:06 +0530

    backend communication integration

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -2,13 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyADFgy3-95164GN-ylbHNVfEkerGh_ihSM",
-    authDomain: "assignment-2efc2.firebaseapp.com",
-    projectId: "assignment-2efc2",
-    storageBucket: "assignment-2efc2.appspot.com",
-    messagingSenderId: "515383083360",
-    appId: "1:515383083360:web:33eeed051e74dc5b605e05",
-    measurementId: "G-4WKH3647Z2"
+    
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
